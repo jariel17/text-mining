177d133db349af503d23d1ef0e07b0c0a45d66c3
Ariel Acosta <j.ariel.acosta17@gmail.com>
2025-05-10 06:52:14 -0500

    Implement POS translation and enhance lemmatization in funciones module

--- funciones.py
+++ funciones.py
@@ -2,14 +2,31 @@ import numpy as np
 import pandas as pd
 import string
 import nltk
+from nltk.corpus import wordnet
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 
-nltk.download('stopwords')
+
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+
 lemmatizer = WordNetLemmatizer()
 
+def translate_pos(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
 
 def preprocess(text):
     
@@ -27,5 +44,14 @@ def preprocess(text):
     tokens = cleaned_text.apply(word_tokenize)
     return tokens
 
-def lemmatize_tokens(tokens):
-    return tokens
+def lemmatize(tokens):
+    lemmatizer = WordNetLemmatizer()
+    tagged_tokens = tokens.apply(pos_tag)
+
+    lemmas = tagged_tokens.apply(
+        lambda tagged_list: [
+            lemmatizer.lemmatize(word, translate_pos(pos))
+            for word, pos in tagged_list
+        ]
+    )
+    return lemmas
